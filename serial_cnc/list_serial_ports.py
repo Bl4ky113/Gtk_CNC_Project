2441d0eccc933bece663441dad529932b37dbd8e
Bl4ky113 <bl4ky@proton.me>
2023-01-02 14:30:33 -0500

    Project Restart

--- serial_cnc/list_serial_ports.py
+++
@@ -1,7 +0,0 @@
-""" Lists the available Serial Comunication Ports, and returns them in a tuple """
-
-from serial.tools import list_ports
-
-def list_serial_ports ():
-    """ Lists the available Serial Comunication Ports, and returns them in a tuple """
-    return tuple(list_ports.comports())
